cb54073bdc5a6121e84f158880e06ac618bf480b
navigaid <navigaid@gmail.com>
2020-03-04 10:28:24 +0800

    keep only singleflight type: "memory"

--- cmd/proxy/actions/app_proxy.go
+++ cmd/proxy/actions/app_proxy.go
@@ -122,27 +122,6 @@ func getSingleFlight(c *config.Config, checker storage.Checker) (stash.Wrapper, 
 	switch c.SingleFlightType {
 	case "", "memory":
 		return stash.WithSingleflight, nil
-	case "etcd":
-		if c.SingleFlight == nil || c.SingleFlight.Etcd == nil {
-			return nil, fmt.Errorf("Etcd config must be present")
-		}
-		endpoints := strings.Split(c.SingleFlight.Etcd.Endpoints, ",")
-		return stash.WithEtcd(endpoints, checker)
-	case "redis":
-		if c.SingleFlight == nil || c.SingleFlight.Redis == nil {
-			return nil, fmt.Errorf("Redis config must be present")
-		}
-		return stash.WithRedisLock(c.SingleFlight.Redis.Endpoint, c.SingleFlight.Redis.Password, checker)
-	case "gcp":
-		if c.StorageType != "gcp" {
-			return nil, fmt.Errorf("gcp SingleFlight only works with a gcp storage type and not: %v", c.StorageType)
-		}
-		return stash.WithGCSLock, nil
-	case "azureblob":
-		if c.StorageType != "azureblob" {
-			return nil, fmt.Errorf("azureblob SingleFlight only works with a azureblob storage type and not: %v", c.StorageType)
-		}
-		return stash.WithAzureBlobLock(c.Storage.AzureBlob, c.TimeoutDuration(), checker)
 	default:
 		return nil, fmt.Errorf("unrecognized single flight type: %v", c.SingleFlightType)
 	}
